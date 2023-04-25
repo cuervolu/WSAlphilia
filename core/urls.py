253bc721bcc76e60116e4fba6659448c729321e2
cuervolu <cuervolu@protonmail.com>
2023-04-25 17:47:55 -0400

    Se agrega paginacion y Router de DRF

--- core/urls.py
+++ core/urls.py
@@ -14,10 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
-from sistema_web_alphilia.views import LibroView
+from django.urls import include, path
+from rest_framework import routers
+from sistema_web_alphilia.views import LibroViewSet
+
+router = routers.DefaultRouter()
+router.register(r'libros', LibroViewSet)
 
 urlpatterns = [
-    path("libros/", LibroView.as_view()),
+    path('', include(router.urls)),
 ]
